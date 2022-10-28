81ebfd3518b3ba21719547313b7ad2428385678c
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2022-10-28 14:08:41 -0400

    added html, css file, and js file. did the HTMl layout, some CSS, and also did some JS

---
+++ script.js
@@ -0,0 +1,18 @@
+var start = document.getElementById("start");
+var time = document.getElementById("timer");
+var secondsLeft == 50;
+
+start.addEventListener("click", function () {
+}
+
+)
+
+
+//need to add event listener for when they click the start buttong
+//maybe add a stop button
+//need to add a timer interval for the timer
+//need to add in the questions
+//need to begin the quiz once they select the start button
+//need to make it so when they select the wrong answer 5 seconds gets deducted
+//need to make it so that if they finish the quiz, the end of the game is displayed
+//need to make it so that if the timer hits 0 before they finish, message displayed you lose pops up
